ccb7eb6cd9632a4d0bade6f08aab156fff05fc2a
Skill Disk <88280791+skilldiskpro@users.noreply.github.com>
2024-10-18 17:56:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+    output_div.innerText = english + "Code"
